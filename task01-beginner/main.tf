8f0187397206848ee9c745f2e90c1c10968dcdd5
stephennwachukwu <stephennwac007@gmail.com>
2024-09-07 11:49:35 +0100

    created an webserver instance and a SG attached to it

--- task01-beginner/main.tf
+++ task01-beginner/main.tf
@@ -6,6 +6,13 @@ provider "aws" {
 resource "aws_instance" "testvm" {
   ami     = "ami-0e86e20dae9224db8"
   instance_type = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.testvm_sg.id}"]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
 
   tags = {
     Name = "testVM-terraform"
@@ -13,3 +20,15 @@ resource "aws_instance" "testvm" {
     }
 }
 
+
+resource "aws_security_group" "testvm_sg" {
+    name = "testVM-terraform-security-group"
+    description = "Security group for testvm instance"
+
+    ingress{
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+      }
+  }
